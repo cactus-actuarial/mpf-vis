69d4a56fa4da6dbef67d95447738c3a9a841e019
cactus <cactusactuarial@gmail.com>
2019-07-01 09:56:09 +0200

    changes to paths instead of files

--- app.R
+++ app.R
@@ -56,30 +56,6 @@ is_long <- function(vector) {
     length(unique(vector)) > 1
 }
 
-rbimodal <- function (n, cpct, mu1, mu2, sig1, sig2) {
-    y0 <- rlnorm(n,mean=mu1, sd = sig1)
-    y1 <- rlnorm(n,mean=mu2, sd = sig2)
-    
-    flag <- rbinom(n,size=1,prob=cpct)
-    y <- y0*(1 - flag) + y1*flag 
-}
-
-# create demo data
-n1 = 5000
-n2 = 6000
-
-demo_data <- as.data.frame(matrix(nrow = (n1 + n2), ncol = 3))
-colnames(demo_data) <- c("MPF", "AGE_AT_ENTRY", "SUM_ASSURED")
-
-demo_data$MPF <- c(rep("1st_MPF", n1), rep("2nd_MPF", n2))
-
-demo_data$AGE_AT_ENTRY <- as.integer(
-                          c(round(rbimodal(n1, 0.2, log(30), log(45), log(1.1), log(1.1)), 0),  
-                            round(rbimodal(n2, 0.2, log(30), log(45), log(1.1), log(1.1)), 0)))
-
-demo_data$SUM_ASSURED  <- c(round(rlnorm(n1, meanlog = log(100), sdlog = log(1.2)), digits = 1)*10^2,
-                            round(rlnorm(n2, meanlog = log(100), sdlog = log(1.2)), digits = 1)*10^2)
-
 ### app
 # ui
 ui <- fluidPage(
@@ -92,40 +68,23 @@ ui <- fluidPage(
                 h4("Load data"),
                 radioButtons(inputId = "no_mpf", label = "Input type:", choices = c("Show 1 MPF" = 1, "Compare 2 MPFs" = 2), 
                              selected = 1, inline = TRUE),
-                
                 hr(),
                 
-                strong("Choose RPT file:"),
+                # 1 MPF
+                # fileInput(inputId = "file_1", label = "1st MPF:", accept = ".rpt"),
+                h4("1st MPF"),
+                textInput(inputId = "path_1", label = "Path:"),
+                selectInput(inputId = "product_1", label = "Product:", choices = c()),
                 
-                checkboxInput(inputId = "sample_data", label = "Use sample data", value = TRUE),
-                
-                
-                # own data, 1 MPF
-                conditionalPanel(condition = "input.sample_data == 0",
-                                 fileInput(inputId = "file_1", label = "1st MPF:", accept = ".rpt")
-                ),
-                
-                # own data, 2 MPFs
-                conditionalPanel(condition = "input.sample_data == 0 && input.no_mpf == 2",
-                                 fileInput(inputId = "file_2", label = "2nd MPF:", accept = ".rpt")
-                ),
-
-                
-                # sample data, 1 MPF
-                conditionalPanel(condition = "input.sample_data == 1 && input.no_mpf == 1",
-                                 htmlOutput(outputId = "path_s")
-                ),
-                
-                # sample data, 2 MPFs
-                conditionalPanel(condition = "input.sample_data == 1 && input.no_mpf == 2",
-                                 htmlOutput(outputId = "path_d")
-                )
+                # 2 MPFs
+                conditionalPanel(condition = "input.no_mpf == 2", h4("2nd MPF")),
+                conditionalPanel(condition = "input.no_mpf == 2", textInput(inputId = "path_2", label = "Path:")),
+                conditionalPanel(condition = "input.no_mpf == 2", selectInput(inputId = "product_2", label = "Product:", choices = c()))
             ),
             
             wellPanel(
                 h4("Graph settings"),
-                selectInput(inputId = "selected_var", label = "Choose variable:", 
-                            choices = c("AGE_AT_ENTRY", "SUM_ASSURED")),
+                selectInput(inputId = "selected_var", label = "Choose variable:", choices = c()),
                 radioButtons(inputId = "hist_type", label = "Histogram type:", choices = c("frequency" = 1, "density" = 2), 
                              selected = 1, inline = TRUE)
             )
@@ -141,11 +100,11 @@ ui <- fluidPage(
                     
                     # no of records in MPF
                     conditionalPanel(condition = "input.no_mpf == 1",
-                                     htmlOutput(outputId = "top_text_s") # single
+                                     htmlOutput(outputId = "no_rows_s") # single
                     ),
                     
                     conditionalPanel(condition = "input.no_mpf == 2",
-                                     htmlOutput(outputId = "top_text_d") # double
+                                     htmlOutput(outputId = "no_rows_d") # double
                     )
                 ),
                 
@@ -162,18 +121,8 @@ ui <- fluidPage(
 
 # server
 server <- function(input, output, session) {
-  
-    ## left panel
-    output$path_s <- renderUI(
-        HTML("<b>1st MPF:</b> C:/Actuarial/YE2018/MPF/IPROD1.rpt")
-    )
 
-    output$path_d <- renderUI(
-        HTML("<b>1st MPF:</b> C:/Actuarial/YE2018/MPF/IPROD1.rpt<br>
-              <b>2nd MPF:</b> C:/Actuarial/YE2017/MPF/IPROD1.rpt")
-    )
-    
-    # show/hide Data_2 tab
+      # show/hide Data_2 tab
     observe({
       if(input$no_mpf == 1) {
         hideTab(inputId = "tabs", target = "Data_2")
@@ -183,42 +132,71 @@ server <- function(input, output, session) {
       }
     })
     
+
+    # get a list of rpt files
+    prod_list_1 <- reactive({
+      path_1 <- input$path_1
+      list.files(path = path_1, pattern = "*.rpt", ignore.case = TRUE)
+    })
+    
+    prod_list_2 <- reactive({
+      path_2 <- input$path_2
+      list.files(path = path_2, pattern = "*.rpt", ignore.case = TRUE)
+    })
+    
+    # update product list choices
+    observe({
+      req(prod_list_1())
+      updateSelectInput(session, "product_1", choices = prod_list_1())
+    })
+    
+    observe({
+      req(prod_list_2())
+      updateSelectInput(session, "product_2", choices = prod_list_2())
+    })
+  
     # load data
     data_mpf <- reactive({
-        # sample data, 1 MPF
-        if (input$no_mpf == 1 && input$sample_data == 1) {
-            data <- subset(demo_data, MPF == "1st_MPF")
-            data
-        # sample data, 2 MPFs
-        } else if (input$no_mpf == 2 && input$sample_data == 1) {
-            data <- demo_data
-            data
-        # own data, 1 MPF
-        } else if (input$no_mpf == 1 && input$sample_data == 0) {
-            inFile <- input$file_1
-            if (is.null(inFile))
-              return(NULL)
-            data <- read_mpf(inFile$datapath)
-            data$MPF <- rep("1st_MPF", dim(data)[1])
-            data
-        # own data, 2 MPFs
-        } else if (input$no_mpf == 2 && input$sample_data == 0) {
-            inFile1 <- input$file_1
-            inFile2 <- input$file_2
-            if (is.null(inFile1) || is.null(inFile2))
-              return(NULL)
-            
-            data_1 <- read_mpf(inFile1$datapath)
-            data_2 <- read_mpf(inFile2$datapath)
-  
-            # set indicators for two datasets
-            data_1$MPF <- rep("1st_MPF", dim(data_1)[1])
-            data_2$MPF <- rep("2nd_MPF", dim(data_2)[1])
-            
-            com_cols <- intersect(colnames(data_1), colnames(data_2))
-            data <- rbind(subset(data_1, select = com_cols), subset(data_2, select = com_cols))
-            data
-        }
+      
+      # 1 MPF
+      if (input$no_mpf == 1) {
+        req(input$path_1)
+        req(input$product_1)
+        
+        datapath_1 <- paste(input$path_1, "\\", input$product_1, sep = "")
+        
+        if (is.null(input$product_1))
+          return(NULL)
+        
+        data <- read_mpf(datapath_1)
+        data$MPF <- rep("1st_MPF", dim(data)[1]) # set indicator for the dataset 
+        data
+        
+      # 2 MPFs
+      } else if (input$no_mpf == 2) {
+        req(input$path_1)
+        req(input$path_2)
+        req(input$product_1)
+        req(input$product_2)
+        
+        datapath_1 <- paste(input$path_1, "\\", input$product_1, sep = "")
+        datapath_2 <- paste(input$path_2, "\\", input$product_2, sep = "")
+        
+        if (is.null(input$product_1) || is.null(input$product_2))
+          return(NULL)
+        
+        data_1 <- read_mpf(datapath_1)
+        data_2 <- read_mpf(datapath_2)
+
+        # set indicators for two datasets
+        data_1$MPF <- rep("1st_MPF", dim(data_1)[1])
+        data_2$MPF <- rep("2nd_MPF", dim(data_2)[1])
+        
+        # choose only common columns
+        com_cols <- intersect(colnames(data_1), colnames(data_2))
+        data <- rbind(subset(data_1, select = com_cols), subset(data_2, select = com_cols))
+        data
+      }
     })
     
     # update variables select list
@@ -272,15 +250,15 @@ server <- function(input, output, session) {
         }
     })
     
-    ## information about number of records
-    output$top_text_s <- renderUI(
+    ## number of records info
+    output$no_rows_s <- renderUI(
       if (is.null(data_mpf())) {}
       else {
         HTML("MPF contains ", nrow(data_mpf()), " records.")
       }
     )
     
-    output$top_text_d <- renderUI(
+    output$no_rows_d <- renderUI(
       if (is.null(data_mpf())) {}
       else {
         HTML("1st MPF contains ", nrow(subset(data_mpf(), MPF == "1st_MPF")), " records.",
